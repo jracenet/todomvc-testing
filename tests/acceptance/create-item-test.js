08761b19a7abaa89a8f46f51a1557bcc4ad8c7ce
Joan Racenet <joan.racenet@hiptest.net>
2018-09-26 11:34:46 +0200

    Add some use cases in create item tests

--- tests/acceptance/create-item-test.js
+++ tests/acceptance/create-item-test.js
@@ -5,6 +5,12 @@ import { setupApplicationTest } from 'ember-qunit';
 module('Acceptance | create item', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /create-item', async function(assert) {
+  test('If no items are created, no list are displayed', async function(assert) {
+  });
+
+  test('When the first item is created, the list appears', async function(assert) {
+  });
+
+  test('When a new item is created, it appears at the bottom of the list', async function(assert) {
   });
 });
